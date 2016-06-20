3cffe8131b9f96828f64a62d1a8e598674cb86a7
Osmir Mariano <osmirmariano@uft.edu.br>
2016-06-20 20:52:05 -0300

    Trabalho aula 12
    Implementações da operações com grafos

---
+++ op_Grafo2Lexicografico.py
@@ -0,0 +1,72 @@
+
+import networkx as nx
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def execute(g,tam):
+    print "Grafo:"
+    P = g.plot()
+    P.show(figsize=tam)
+
+    laplaciana = g.kirchhoff_matrix() #laplaciana
+    polinomio = laplaciana.charpoly() #polinomio caracteristico
+    autovalores = laplaciana.eigenvalues() #autovalores
+
+    #garantindo o aredondamento dos autovalores
+    aut = []
+    for i in range(len(autovalores)):
+        aut.append(round(autovalores[i],5))
+    aut = sorted(aut)
+    print "AUTOVALORES"
+    print aut
+
+    graus = g.degree_sequence(); #grau
+    graus.sort();
+    print "Graus:"
+    print graus
+    print "Grau minimo:"
+    print min(graus) #grau min
+    print "Grau maximo:"
+    print max(graus) #grau max
+    if len(aut) > 1: #printando a(G)
+        print "Conectividade algebrica"
+        print aut[1]
+
+    #calculando o tamanho do eixo X
+    quantPontos =[]
+    for i in range(len(graus)):
+        quantPontos.append(i)
+    #calculando o tamanho do eixo Y
+    aux = []
+    if graus[len(graus)-1] > autovalores[len(graus)-1]:
+        aux = graus[len(graus)-1] +2
+    else:
+        aux = autovalores[len(graus)-1] +2
+
+    plt.clf()
+    titulo = plt.plot(quantPontos, aut,linestyle="dashed", marker="o", color="blue",label="Autovalores") #plotando os autovalores no grafico
+    titulo = plt.plot(quantPontos, graus,linestyle="dashed", marker="o", color="red",label="Grau dos vertices") #plotando os graus dos vertices no grafico
+    titulo = plt.legend(loc='upper center', bbox_to_anchor=(0.45, -0.025, .45,1.17),fancybox=True, shadow=True, ncol=3) #configurando a legenda
+    titulo = plt.xlabel("Indice") #legenda para o eixo X
+    titulo = plt.ylabel("Valores") #legenda para o eixo Y
+    titulo = plt.title("Graus dos vertices e Autovalores (por indice)") #titulo para o grafico
+    plt.grid(True) #ativando a grade no grafico
+    plt.show() #mostrando o grafico
+    plt.clf()
+    print "\n\n\n\n\n\n\n"
+
+
+#CALCULANDO GRAFO 2 LEXICOGRÁFICO - AULA 12 
+#---------------------------------------------------------------------------------------
+g = Graph({'AE':['BE','CE','CF','BF','CG','BG'],'BE':['AE','AF','AG','BE','CE','CG'],'CE':['AE','BE','AF','AG','BF','BG'],'AF':['BE','CE','CF','BF','CG','AG'],'BF':['AE','AF','AG','BG','CG','CF','CE'],'CF':['BE','AE','BF','AF','AG','BG','CG'],'AG':['AF','BE','CE','BF','CF','CG','BG'],'BG':['AF','AE','BF','CF','CG','CG'],'CG':['AG','BG','AF','AE','BF','BE','CF']})
+execute(g,[3,3])
+
+
+
+
+
+
+
+
+
---
+++ op_Grafo3Contracao.py
@@ -0,0 +1,72 @@
+
+import networkx as nx
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def execute(g,tam):
+    print "Grafo:"
+    P = g.plot()
+    P.show(figsize=tam)
+
+    laplaciana = g.kirchhoff_matrix() #laplaciana
+    polinomio = laplaciana.charpoly() #polinomio caracteristico
+    autovalores = laplaciana.eigenvalues() #autovalores
+
+    #garantindo o aredondamento dos autovalores
+    aut = []
+    for i in range(len(autovalores)):
+        aut.append(round(autovalores[i],5))
+    aut = sorted(aut)
+    print "AUTOVALORES"
+    print aut
+
+    graus = g.degree_sequence(); #grau
+    graus.sort();
+    print "Graus:"
+    print graus
+    print "Grau minimo:"
+    print min(graus) #grau min
+    print "Grau maximo:"
+    print max(graus) #grau max
+    if len(aut) > 1: #printando a(G)
+        print "Conectividade algebrica"
+        print aut[1]
+
+    #calculando o tamanho do eixo X
+    quantPontos =[]
+    for i in range(len(graus)):
+        quantPontos.append(i)
+    #calculando o tamanho do eixo Y
+    aux = []
+    if graus[len(graus)-1] > autovalores[len(graus)-1]:
+        aux = graus[len(graus)-1] +2
+    else:
+        aux = autovalores[len(graus)-1] +2
+
+    plt.clf()
+    titulo = plt.plot(quantPontos, aut,linestyle="dashed", marker="o", color="blue",label="Autovalores") #plotando os autovalores no grafico
+    titulo = plt.plot(quantPontos, graus,linestyle="dashed", marker="o", color="red",label="Grau dos vertices") #plotando os graus dos vertices no grafico
+    titulo = plt.legend(loc='upper center', bbox_to_anchor=(0.45, -0.025, .45,1.17),fancybox=True, shadow=True, ncol=3) #configurando a legenda
+    titulo = plt.xlabel("Indice") #legenda para o eixo X
+    titulo = plt.ylabel("Valores") #legenda para o eixo Y
+    titulo = plt.title("Graus dos vertices e Autovalores (por indice)") #titulo para o grafico
+    plt.grid(True) #ativando a grade no grafico
+    plt.show() #mostrando o grafico
+    plt.clf()
+    print "\n\n\n\n\n\n\n"
+
+
+#CALCULANDO GRAFO 3 CONTRAÇÃO - AULA 12 
+#---------------------------------------------------------------------------------------
+g = Graph({'1':['2','5'],'2':['5','8','3'],'3':['2','4'],'4':['5','76'],'5':['1','2','8','76']})
+execute(g,[3,3])
+
+
+
+
+
+
+
+
+
